1bdf5e1cd8c4d6d36446b2231f0bcf279633c1b2
takumin <takumiiinn@gmail.com>
2014-12-23 12:51:04 +0900

    fix

--- manifests/config.pp
+++ manifests/config.pp
@@ -41,18 +41,21 @@ class freebsd_nfs::config {
     if $::freebsd_nfs::mountd_flags != '' {
       sysrc { 'rpcbind_flags':
         value => "$::freebsd_nfs::rpcbind_flags",
+        path  => '/etc/rc.conf.d/rpcbind',
       }
     }
 
     if $::freebsd_nfs::rpc_lockd_flags != '' {
       sysrc { 'rpc_lockd_flags':
         value => "$::freebsd_nfs::rpc_lockd_flags",
+        path  => '/etc/rc.conf.d/lockd',
       }
     }
 
     if $::freebsd_nfs::rpc_statd_flags != '' {
       sysrc { 'rpc_statd_flags':
         value => "$::freebsd_nfs::rpc_statd_flags",
+        path  => '/etc/rc.conf.d/statd',
       }
     }
 
@@ -60,10 +63,16 @@ class freebsd_nfs::config {
       if $::freebsd_nfs::nfsuserd_flags != '' {
         if $::freebsd_nfs::nfsuserd_flags !~ /-domain/ {
           $nfsuserd_flags = "${::freebsd_nfs::nfsuserd_flags} -domain ${::domain}"
+        } else {
+          $nfsuserd_flags = ${::freebsd_nfs::nfsuserd_flags}
         }
-        sysrc { 'nfsuserd_flags':
-          value => "$nfsuserd_flags",
-        }
+      } else {
+          $nfsuserd_flags = "-domain ${::domain}"
+      }
+
+      sysrc { 'nfsuserd_flags':
+        value => "$nfsuserd_flags",
+        path  => '/etc/rc.conf.d/nfsuserd',
       }
     }
   }
@@ -72,41 +81,48 @@ class freebsd_nfs::config {
     if $::freebsd_nfs::mountd_flags != '' {
       sysrc { 'mountd_flags':
         value => "$::freebsd_nfs::mountd_flags",
+        path  => '/etc/rc.conf.d/mountd',
       }
     }
 
     if $::freebsd_nfs::weak_mountd_authentication {
       sysrc { 'weak_mountd_authentication':
         value => 'YES',
+        path  => '/etc/rc.conf.d/mountd',
       }
     }
 
     if $::freebsd_nfs::nfs_server_flags != '' {
       sysrc { 'nfs_server_flags':
         value => "$::freebsd_nfs::nfs_server_flags",
+        path  => '/etc/rc.conf.d/nfsd',
       }
     }
 
     if $::freebsd_nfs::nfs_reserved_port_only != '' {
       sysrc { 'nfs_reserved_port_only':
         value => 'YES',
+        path  => '/etc/rc.conf.d/nfsd',
       }
     }
 
     if $::freebsd_nfs::oldnfs_server_enable != '' {
       sysrc { 'oldnfs_server_enable':
         value => 'YES',
+        path  => '/etc/rc.conf.d/nfsd',
       }
     }
 
     if $::freebsd_nfs::use_nfsv4 {
       sysrc { 'nfsv4_server_enable':
         value => 'YES',
+        path  => '/etc/rc.conf.d/nfsd',
       }
     } else {
       if $::freebsd_nfs::nfsv4_server_enable {
         sysrc { 'nfsv4_server_enable':
           value => 'YES',
+          path  => '/etc/rc.conf.d/nfsd',
         }
       }
     }
